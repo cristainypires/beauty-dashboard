6d32efee1419e9575aae589e173961f3b99bbce4
crist <cjpires@uta.cv>
2026-01-31 01:13:15 -0100

    x

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports ={
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}", // Isso cobre tudo dentro de src (pages, components, etc)
   ],
